0c9ca8ad71fe1d21eea5cf75b6187772b4e0f23c
claytantor <claytantor@gmail.com>
2014-05-24 22:05:19 +0000

    trying to make github the sync

--- gitpatron/settings.py
+++ gitpatron/settings.py
@@ -69,7 +69,7 @@ DATABASES = {
         'NAME': 'gitpatron',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': 'gitpatron',
-        'PASSWORD': 'gitpatron',
+        'PASSWORD': 'New89Govern',
         'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',                      # Set to empty string for default.
     }
